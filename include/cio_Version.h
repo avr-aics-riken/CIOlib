d96d1a8950dc9fe0a35df09534acd3707a45230a
Kenji Ono <keno@riken.jp>
2014-03-04 11:42:55 +0900

    r43 version format [ver 1.5.5]

--- include/cio_Version.h
+++ include/cio_Version.h
@@ -15,10 +15,10 @@
 #define _CIO_VERSION_H_
 
 /** CIOライブラリのバージョン */
-#define CIO_VERSION_NO  "1.5.4"
+#define CIO_VERSION_NO  "1.5.5"
 
 /** CIOライブラリのリビジョン */
-#define CIO_REVISION    "20140218_1600"
+#define CIO_REVISION    "20140304_1200"
 
 #endif /* _CIO_VERSION_H_ */
 
